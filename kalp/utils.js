70ef4762c613ff133149cd41c0b66ef0c10c5cf7
kalp-cg <kalp.patel.codinggita@gmail.com>
2026-07-08 23:30:03 +0530

    fix/null-check: add helper methods for session ID and username formatting in kalp folder

--- kalp/utils.js
+++ kalp/utils.js
@@ -8,7 +8,20 @@ function calculateHash(data) {
   return crypto.createHash('sha256').update(data).digest('hex');
 }
 
+// WARNING: Weak/Insecure session ID generation using Math.random()
+function generateSessionId() {
+  return 'sess-' + Math.random().toString(36).substring(2, 15);
+}
+
+// WARNING: Null dereference vulnerability (calling .toUpperCase() on input that can be null/undefined)
+function formatUsername(userObj) {
+  const name = userObj.username;
+  return name.toUpperCase();
+}
+
 module.exports = {
   generateSecureToken,
-  calculateHash
+  calculateHash,
+  generateSessionId,
+  formatUsername
 };
